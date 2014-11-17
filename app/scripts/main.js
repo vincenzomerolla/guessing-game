152b7192887e8c12d45d60e47fc6254b2527252b
Vincenzo Merolla <vincenzomerolla@aol.com>
2014-11-17 12:08:18 -0500

    Added up/down arrow next to guess to show guess higher/lower

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,13 @@
 'use strict';
 
 $(function () {
-  var randNumber;
-  var numGuesses = 5;
+  var randNumber = null;
+  var numGuesses = 0;
   var submit = $('#submit-guess');
   var hint = $('#hint');
   var guess = $('#guess');
   var playAgain = $('#play-again');
+  var fillerText = $('.filler-text');
   var gameForm = $('.game-form');
   var guessesLeft = $('#guesses-left');
   var guesses = [];
@@ -19,8 +20,8 @@ $(function () {
     console.log('generated a random number: ' + randNumber);
   }
 
-  function checkValidity(input) {
-    if ( input > 0 && input < 101 ) {
+  function isValidNumber(input) {
+    if ( !isNaN(input) && input > 0 && input < 101 ) {
       return true;
     }
     return false;
@@ -29,30 +30,43 @@ $(function () {
   function restartGame() {
     numberRow.find('td:gt(0)').remove();
     levelRow.find('td:gt(0)').remove();
-    $('.filler-text').toggleClass('hide');
+    if (fillerText.hasClass('hide')) {
+      fillerText.removeClass('hide');
+    } 
     submit.prop({ disabled: false });
-    playAgain.prop({ disabled: true });
+    hint.prop({ disabled: true });
     genRandNumber();
     guess.val('');
     guesses = [];
-    numGuesses = 5;
-    guessesLeft.text(numGuesses);
+    numGuesses = 0;
+    guessesLeft.text(5);
     hint.data('bs.popover').options.content = randNumber;
   }
 
   function endGame() {
     submit.prop({ disabled: true });
-    playAgain.prop({ disabled: false });
+  }
+
+  // function repeat 
+  // http://stackoverflow.com/questions/202605/repeat-string-javascript
+  function repeat(pattern, count) {
+    if (count < 1) return '';
+    var result = '';
+    while (count > 1) {
+        if (count & 1) result += pattern;
+        count >>= 1, pattern += pattern;
+    }
+    return result + pattern;
   }
 
   // Initialization for Bootstrap javascript elements
   genRandNumber();
   hint.popover({ content: randNumber});
-  guessesLeft.text(numGuesses);
+  guessesLeft.text(5);
 
   // Submit button click event
   submit.click(function(event) {
-    if ( checkValidity(guess.val()) ) {
+    if ( isValidNumber(guess.val()) ) {
       var guessedNumber = Number(guess.val());
       if ( guessedNumber === randNumber) {
         $.growl('<strong>'+ guess.val() + '</strong> is the correct answer! You have won the game!', {
@@ -67,29 +81,38 @@ $(function () {
           type: 'danger'
         });
       } else {
-        if (numGuesses === 5) { $('.filler-text').toggleClass('hide');}
+        if (numGuesses === 0) { 
+          hint.prop({ disabled: false });
+          fillerText.toggleClass('hide');
+        }
         guesses.push(guessedNumber);
-        numGuesses--;
-        guessesLeft.text(numGuesses);
-        numberRow.append('<td>'+guessedNumber+'</td>');
+        numGuesses++;
+        guessesLeft.text(5 - numGuesses);
+        
+        var str = '<td>'+guessedNumber+'&nbsp;';
+             
+        str += guessedNumber < randNumber ? '<span class="ion-arrow-up-a"></span></td>' : '<span class="ion-arrow-down-a"></span></td>';
+  
+
+        numberRow.append(str);
 
         var diff = Math.abs(randNumber - guessedNumber);
         if (diff < 5) {
-          levelRow.append('<td><span class="red ion-fireball"></span><span class="red ion-fireball"></span><span class="red ion-fireball"></span></td>');
+          levelRow.append('<td>' + repeat('<span class="red ion-fireball"></span>', 3) + '</td>');
         } else if (diff < 10) {
-          levelRow.append('<td><span class="red ion-fireball"></span><span class="red ion-fireball"></span></td>');
+          levelRow.append('<td>'+ repeat('<span class="red ion-fireball"></span>', 2) +'</td>');
         } else  if (diff < 15) {
           levelRow.append('<td><span class="red ion-fireball"></span></td>');
         } else  if (diff < 20) {
           levelRow.append('<td><span class="blue ion-cube"></span></td>');
         } else  if (diff < 25) {
-          levelRow.append('<td><span class="blue ion-cube"></span><span class="blue ion-cube"></span></td>');
+          levelRow.append('<td>' + repeat('<span class="blue ion-cube"></span>', 2) + '</td>');
         } else  {
-          levelRow.append('<td><span class="blue ion-cube"></span><span class="blue ion-cube"></span><span class="blue ion-cube"></span></td>');
+          levelRow.append('<td>' + repeat('<span class="blue ion-cube"></span>', 3) + '</td>');
         }
 
 
-        if (numGuesses === 0) { 
+        if (numGuesses === 5) { 
           $.growl('Sorry, but the correct answer was <strong>' + randNumber + '</strong>.', {
             allow_dismiss: false,
             type: 'danger'
@@ -107,7 +130,7 @@ $(function () {
 
   // Input enter keypress --- triggers submit button click event
   guess.keypress(function(e) {
-    if(e.which === 13 && numGuesses !== 0) {
+    if(e.which === 13 && numGuesses !== 5) {
       submit.trigger('click');
     }
   });
@@ -115,11 +138,15 @@ $(function () {
   // Play again button resets the game
   playAgain.click(function(event) {
     restartGame();
+    $.growl('The game has been reset.', {
+      allow_dismiss: false,
+      type: 'warning'
+    });
   });
 
   // On click listener to close out popover
   $('html').on('click', function(e) {
-    if (typeof $(e.target).data('original-title') == 'undefined') {
+    if (typeof $(e.target).data('original-title') === 'undefined') {
       $('[data-original-title]').popover('hide');
     }
   });
